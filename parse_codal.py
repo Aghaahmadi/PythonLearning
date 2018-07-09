0644aa1eca360abda0f97ac6a8eb9dfd26fc3547
Aghaahmadi <ahmadi@gmail.com>
2018-07-09 22:51:29 +0430

    add header

--- parse_codal.py
+++ parse_codal.py
@@ -1,9 +1,13 @@
 import pandas as pd
 url = "http://codal.ir/Reports/Decision.aspx?LetterSerial=lOCU3hBfbCpdGw8UrgfDbw%3d%3d&rt=0&let=58&ct=0"
-table = pd.read_html(url, encoding='UTF-8')
+read_table = pd.read_html(url, encoding='UTF-8')
 
-hd = table[1].values[1]
+hd = read_table[1].values[1]
 headers = []
 for h in hd:
     headers.append(h)
+
+table = read_table[2]
+
+table.columns = headers
 
